39521870f4dfa9134c10d1d2a7e3cd04b7c4b26e
VeronicaLil <105051293+VeronicaLil@users.noreply.github.com>
2022-06-03 14:39:11 +0200

    update main.py

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ import sys
 if __name__ == "__main__":
     n_1 = int(sys.argv[1])
     n_2 = int(sys.argv[2])
-    print(f"operazione {n_1} * {n_2} = ", n_1 * n_2)
-    print(f"operazione {n_1} * {n_2} = ", n_1 + n_2)
+    op = str(sys.agrv[3])
+    if op == "add":
+        print(f"operazione {n_1} + {n_2} = ", n_1 + n_2)
+    elif op == "mol":
+        print(f"operazione {n_1} * {n_2} = ", n_1 * n_2)
+    elif op == "sub":
+        print(f"operazione {n_1} - {n_2} = ", n_1 - n_2")
+    else:
+        print(f"operazione {n_1} / {n_2} = ", n_1 / n_2")
