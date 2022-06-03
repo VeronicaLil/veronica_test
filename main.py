e6ce09bb9cc455c4f56364cc3c550eb0a39b17e0
demian2435 <demian2435@gmail.com>
2022-06-03 14:23:13 +0200

    update main

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ if __name__ == "__main__":
     n_1 = int(sys.argv[1])
     n_2 = int(sys.argv[2])
     print(f"operazione {n_1} * {n_2} = ", n_1 * n_2)
+    print(f"operazione {n_1} * {n_2} = ", n_1 + n_2)
