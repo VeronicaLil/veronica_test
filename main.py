0f29c9fc1047da7d425787410fd0473c3870d572
demian2435 <demian2435@gmail.com>
2022-06-03 12:48:08 +0200

    added main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import sys
+
+# argv[0] nome programma ... gli altri i valori da console
+if __name__ == "__main__":
+    n_1 = int(sys.argv[1])
+    n_2 = int(sys.argv[2])
+    print(f"operazione {n_1} * {n_2} = ", n_1 * n_2)
